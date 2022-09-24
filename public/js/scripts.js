52f240a9b41bc39950aae60ce68a1d290cfc00fc
arks143 <93473111+arks143@users.noreply.github.com>
2022-09-24 14:38:56 +0100

    Metamask integration

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,11 +1,12 @@
-function copyTextDiv() {
-  var copyText = document.getElementById("copy-text");
+function copyTextDiv(elementid,copy_tooltip) {
+  var copyText = document.getElementById(elementid);
   navigator.clipboard.writeText(copyText.innerHTML);
-  var tooltip = document.getElementById("copy-tooltip");
-  tooltip.innerHTML = "Copied: " + copyText.innerHTML;
+  var tooltip = document.getElementById(copy_tooltip);
+  // tooltip.innerHTML = "Copied: " + copyText.innerHTML;
+    tooltip.innerHTML = "Copied ";
 }
 
-function outText() {
-  var tooltip = document.getElementById("copy-tooltip");
-  tooltip.innerHTML = "Click to Copy";
+function outText(tooltipID) {
+  var tooltip = document.getElementById(tooltipID);
+  tooltip.innerHTML = "Copy";
 }
